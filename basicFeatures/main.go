afe2fd1900d14738b8ebeff5b034db5bbe76f51b
ensofter <bogestvenniyiri@yandex.ru>
2022-08-30 01:14:24 +0300

    rand/int()

--- basicFeatures/main.go
+++ basicFeatures/main.go
@@ -7,4 +7,14 @@ import (
 
 func main() {
 	fmt.Println("Value:", rand.Int())
+	fmt.Println("Hello, GO!")
+	fmt.Println(20 + 20)
+	fmt.Println(30 + 20)
+
+	const price float32 = 275.00
+	const tax float32 = 27.50
+	fmt.Println(price + tax)
+
+	const quantity = 2
+	fmt.Println("Total:", quantity*(price+tax))
 }
