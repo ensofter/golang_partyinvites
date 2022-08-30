010ae1261a93d57c8b7ab7ee08ae8b92a4a01263
ensofter <bogestvenniyiri@yandex.ru>
2022-08-31 00:17:24 +0300

    Pointers

--- basicFeatures/main.go
+++ basicFeatures/main.go
@@ -17,4 +17,15 @@ func main() {
 
 	const quantity = 2
 	fmt.Println("Total:", quantity*(price+tax))
+
+	const poop, peee float32 = 275, 27.50
+	const one, two = 2, true
+	fmt.Println("Total:", 2*one*(poop+peee))
+	fmt.Println("In stock: ", two)
+
+	var myfirst float32 = 275.00
+	var mysecond float32 = 27.50
+	fmt.Println(myfirst * mysecond)
+	myfirst = 300
+	fmt.Println(myfirst + mysecond)
 }
