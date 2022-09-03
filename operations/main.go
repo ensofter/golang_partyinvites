eea80120c005e26018ebf5e10a041ed1b7dfd44d
ensofter <bogestvenniyiri@yandex.ru>
2022-09-03 15:48:55 +0300

    Parsing strings

--- operations/main.go
+++ operations/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strconv"
 )
 
 func main() {
-	first := 100
-	const second = 200.00
+	val1, val2, val3 := "true", "false", "true"
 
-	eaual := first == second
-	fmt.Println(eaual)
-	fmt.Println(math.Floor(27.1))
+	bool1, b1err := strconv.ParseBool(val1)
+	bool2, b2err := strconv.ParseBool(val2)
+	if bool3, b3err := strconv.ParseBool(val3); b3err != nil {
+		panic(b3err)
+	} else {
+		fmt.Println("Bool 3", bool3, b3err)
+
+	}
+
+	fmt.Println("Bool 1", bool1, b1err)
+	fmt.Println("Bool 2", bool2, b2err)
 }
