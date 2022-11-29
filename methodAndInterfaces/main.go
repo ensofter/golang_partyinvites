e10d66cab48212d3b8b6a3b43741f08bb9724c7e
Romashov Aleksey <aromashov@ozon.ru>
2022-11-29 14:48:31 +0300

    Add interface

---
+++ methodAndInterfaces/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Expense interface {
+	getName() string
+	getCost(annual bool) float64
+}
+
+type Service struct {
+	description    string
+	durationMonths int
+	monthlyFee     float64
+}
+
+func (s Service) getName() string {
+	return s.description
+}
+
+func (s Service) getCost(recur bool) float64 {
+	if recur {
+		return s.monthlyFee * float64(s.durationMonths)
+	}
+	return s.monthlyFee
+}
+
+type Product struct {
+	name, category string
+	price          float64
+}
+
+func (p Product) getName() string {
+	return p.name
+}
+
+func (p Product) getCost(_ bool) float64 {
+	return p.price
+}
+
+func main() {
+
+	kayak := Product{"Kayak", "Watersports", 275}
+	insurance := Service{"Boat Cover", 12, 89.50}
+
+	fmt.Println("Product:", kayak.name, "Price:", kayak.price)
+	fmt.Println("Service:", insurance.description, "Price:", insurance.monthlyFee*float64(insurance.durationMonths))
+}
