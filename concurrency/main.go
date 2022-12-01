37682a2b3cb1110e56e51704847c559101d276b6
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2022-12-01 22:37:24 +0300

    concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("Main function started")
 	CalcStoreTotal(Products)
-	time.Sleep(time.Second * 5)
 	fmt.Println("main function complete")
 }
--- concurrency/operations.go
+++ concurrency/operations.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
-	//var channel chan float64 = make(chan float64)
+	var channel = make(chan float64)
 	for category, group := range data {
-		storeTotal += group.TotalPrice(category)
+		go group.TotalPrice(category, channel)
+	}
+	for i := 0; i < len(data); i++ {
+		fmt.Println("-- channel read pending")
+		value := <-channel
+		fmt.Println("-- channel read complete", value)
+		storeTotal += value
+		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
 
-func (group ProductGroup) TotalPrice(category string) (total float64) {
+func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
+	var total float64
 	for _, p := range group {
-		fmt.Println(category, "product:", p.Name)
+		//fmt.Println(category, "product:", p.Name)
 		total += p.Price
-		//time.Sleep(time.Millisecond * 100)
+		time.Sleep(time.Millisecond * 100)
 	}
-	fmt.Println(category, "subtotal:", ToCurrency(total))
-	return
+	fmt.Println(category, "channel sending", ToCurrency(total))
+	resultChannel <- total
+	fmt.Println(category, "channel send complete")
 }
